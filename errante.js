336f08e5339f79ee55c35ec0c932d987faedc0e2
Nicolasvitto <156943718+Nicolasvitto@users.noreply.github.com>
2024-05-23 10:23:17 -0300

    Delete errante.js

--- errante.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-	const galeria = document.querySelector(".galeria");
-	galeria.addEventListener("click", function(event) {
-		if (event.target.tagName === "IMG") {
-			
-			console.log("Clique na imagem:", event.target.src);
-		}
-	});
-});
